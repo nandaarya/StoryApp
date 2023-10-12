e0118ac76b264bf3a58e49847a200220600ddd77
Nanda Arya Putra <nandaarya404@gmail.com>
2023-10-12 23:57:14 +0700

    add passwordEditText customview (still have bug can't see password)

---
+++ app/src/main/java/com/example/storyapp/ui/customview/PasswordEditText.kt
@@ -0,0 +1,74 @@
+package com.example.storyapp.ui.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.example.storyapp.R
+
+class PasswordEditText : AppCompatEditText {
+    private lateinit var iconPassword: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+        transformationMethod = PasswordTransformationMethod.getInstance()
+    }
+
+    private fun init() {
+        iconPassword = ContextCompat.getDrawable(context, R.drawable.ic_baseline_lock_24) as Drawable
+        compoundDrawablePadding = 12
+        setIconsDrawable(iconPassword)
+        setHint(R.string.edt_password_hint)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty() && s.length < 8) passwordEditTextLayout.error = context.getString(R.string.invalid_password)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+    }
+
+    private fun setIconsDrawable(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText
+        )
+    }
+}
