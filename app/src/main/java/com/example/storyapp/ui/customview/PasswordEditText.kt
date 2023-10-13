fee8fe0ad17bb0b27bac652f19ccb389857e21de
Nanda Arya Putra <nandaarya404@gmail.com>
2023-10-13 15:19:26 +0700

    fix bug view password

--- app/src/main/java/com/example/storyapp/ui/customview/PasswordEditText.kt
+++ app/src/main/java/com/example/storyapp/ui/customview/PasswordEditText.kt
@@ -1,12 +1,14 @@
 package com.example.storyapp.ui.customview
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
 import android.text.Editable
+import android.text.InputType
 import android.text.TextWatcher
-import android.text.method.PasswordTransformationMethod
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.content.ContextCompat
@@ -14,6 +16,7 @@ import com.example.storyapp.R
 
 class PasswordEditText : AppCompatEditText {
     private lateinit var iconPassword: Drawable
+    private var isPasswordVisible = false
 
     constructor(context: Context) : super(context) {
         init()
@@ -34,14 +37,26 @@ class PasswordEditText : AppCompatEditText {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         textAlignment = View.TEXT_ALIGNMENT_VIEW_START
-        transformationMethod = PasswordTransformationMethod.getInstance()
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun init() {
         iconPassword = ContextCompat.getDrawable(context, R.drawable.ic_baseline_lock_24) as Drawable
         compoundDrawablePadding = 12
         setIconsDrawable(iconPassword)
         setHint(R.string.edt_password_hint)
+
+        setOnTouchListener { _, event ->
+            val drawableRight = 2 // Right drawable index
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= (right - compoundDrawables[drawableRight].bounds.width())) {
+                    // Toggle password visibility
+                    togglePasswordVisibility()
+                    return@setOnTouchListener true
+                }
+            }
+            false
+        }
 
         addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
@@ -49,8 +64,11 @@ class PasswordEditText : AppCompatEditText {
             }
 
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                if (s.toString().isNotEmpty() && s.length < 8)
-                    error = context.getString(R.string.invalid_password)
+                if (s.toString().isNotEmpty() && s.length < 8) {
+                    setError("Password tidak boleh kurang dari 8 karakter", null)
+                } else {
+                    error = null
+                }
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -71,5 +89,15 @@ class PasswordEditText : AppCompatEditText {
             endOfTheText,
             bottomOfTheText
         )
+    }
+
+    private fun togglePasswordVisibility() {
+        isPasswordVisible = !isPasswordVisible
+        inputType = if (isPasswordVisible) {
+            InputType.TYPE_CLASS_TEXT
+        } else {
+            InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        }
+        setSelection(text!!.length)
     }
 }
